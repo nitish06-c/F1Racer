0d28ef1d40990b5f427ccf8439a77fdd3a84b715
Nitish Madabusi <62602950+nitish06-c@users.noreply.github.com>
2022-01-07 23:34:20 -0500

    Add files via upload

---
+++ Countdown.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject CountDown;
+    public AudioSource GetReady;
+    public AudioSource GoAudio;
+    public GameObject LapTimer;
+    public GameObject CarControls;
+
+    void Start()
+    {
+       
+
+    }
+
+}
